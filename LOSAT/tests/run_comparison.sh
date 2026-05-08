b5257e72313ae29fa4ee29473aed351dbae69590
satoshikawato <genome.kawato@gmail.com>
2026-05-08 12:47:26 +0900

    Update run_comparison.sh

--- LOSAT/tests/run_comparison.sh
+++ LOSAT/tests/run_comparison.sh
@@ -56,53 +56,8 @@ echo "Starting LOSAT commands..."
 (time $LOSAT_BIN blastn -q ./fasta/AP027202.fasta -s ./fasta/LC738875.fasta -o ./losat_out/MjPMNV.MlPMNV.losatn.blastn.out --task blastn -n 1 )&>./losat_out/MjPMNV.MlPMNV.losatn.blastn.log
 
 
-
-# --- TLOSATX Commands (Genetic Code: 1) ---
-echo "Starting AP027280 self..."
-(time $LOSAT_BIN tblastx -q ./fasta/AP027280.fasta -s ./fasta/AP027280.fasta -o ./losat_out/AP027280.AP027280.tlosatx.n1.out --query-gencode 1 --db-gencode 1 -n 1)&>./losat_out/AP027280.AP027280.tlosatx.n1.log 
-(time $LOSAT_BIN tblastx -q ./fasta/AP027280.fasta -s ./fasta/AP027280.fasta -o ./losat_out/AP027280.AP027280.tlosatx.n8.out --query-gencode 1 --db-gencode 1 -n 8 )&>./losat_out/AP027280.AP027280.tlosatx.n8.log 
-
-# MjeNMV vs MelaMJNV
-echo "Starting MjeNMV vs MelaMJNV (LOSAT)..."
-(time $LOSAT_BIN tblastx -q ./fasta/MjeNMV.fasta -s ./fasta/MelaMJNV.fasta -o ./losat_out/MjeNMV.MelaMJNV.tlosatx.n8.out --query-gencode 1 --db-gencode 1 -n 8 ) &> ./losat_out/MjeNMV.MelaMJNV.tlosatx.n8.log 
-
-# MelaMJNV vs PemoMJNVA
-echo "Starting MelaMJNV vs PemoMJNVA (LOSAT)..."
-(time $LOSAT_BIN tblastx -q ./fasta/MelaMJNV.fasta -s ./fasta/PemoMJNVA.fasta -o ./losat_out/MelaMJNV.PemoMJNVA.tlosatx.n8.out --query-gencode 1 --db-gencode 1 -n 8 ) &> ./losat_out/MelaMJNV.PemoMJNVA.tlosatx.n8.log 
-
-# PemoMJNVA vs PeseMJNV
-echo "Starting PemoMJNVA vs PeseMJNV (LOSAT)..."
-(time $LOSAT_BIN tblastx -q ./fasta/PemoMJNVA.fasta -s ./fasta/PeseMJNV.fasta -o ./losat_out/PemoMJNVA.PeseMJNV.tlosatx.n8.out --query-gencode 1 --db-gencode 1 -n 8 ) &> ./losat_out/PemoMJNVA.PeseMJNV.tlosatx.n8.log 
-
-# PeseMJNV vs PemoMJNVB
-echo "Starting PeseMJNV vs PemoMJNVB (LOSAT)..."
-(time $LOSAT_BIN tblastx -q ./fasta/PeseMJNV.fasta -s ./fasta/PemoMJNVB.fasta -o ./losat_out/PeseMJNV.PemoMJNVB.tlosatx.n8.out --query-gencode 1 --db-gencode 1 -n 8 ) &> ./losat_out/PeseMJNV.PemoMJNVB.tlosatx.n8.log 
-
-# PemoMJNVB vs LvMJNV
-echo "Starting PemoMJNVB vs LvMJNV (LOSAT)..."
-(time $LOSAT_BIN tblastx -q ./fasta/PemoMJNVB.fasta -s ./fasta/LvMJNV.fasta -o ./losat_out/PemoMJNVB.LvMJNV.tlosatx.n8.out --query-gencode 1 --db-gencode 1 -n 8 ) &> ./losat_out/PemoMJNVB.LvMJNV.tlosatx.n8.log 
-
-# LvMJNV vs TrcuMJNV
-echo "Starting LvMJNV vs TrcuMJNV (LOSAT)..."
-(time $LOSAT_BIN tblastx -q ./fasta/LvMJNV.fasta -s ./fasta/TrcuMJNV.fasta -o ./losat_out/LvMJNV.TrcuMJNV.tlosatx.n8.out --query-gencode 1 --db-gencode 1 -n 8 ) &> ./losat_out/LvMJNV.TrcuMJNV.tlosatx.n8.log 
-
-# TrcuMJNV vs MellatMJNV
-echo "Starting TrcuMJNV vs MellatMJNV (LOSAT)..."
-(time $LOSAT_BIN tblastx -q ./fasta/TrcuMJNV.fasta -s ./fasta/MellatMJNV.fasta -o ./losat_out/TrcuMJNV.MellatMJNV.tlosatx.n8.out --query-gencode 1 --db-gencode 1 -n 8 ) &> ./losat_out/TrcuMJNV.MellatMJNV.tlosatx.n8.log 
-
-# MellatMJNV vs MeenMJNV
-echo "Starting MellatMJNV vs MeenMJNV (LOSAT)..."
-(time $LOSAT_BIN tblastx -q ./fasta/MellatMJNV.fasta -s ./fasta/MeenMJNV.fasta -o ./losat_out/MellatMJNV.MeenMJNV.tlosatx.n8.out --query-gencode 1 --db-gencode 1 -n 8 ) &> ./losat_out/MellatMJNV.MeenMJNV.tlosatx.n8.log 
-
-# MeenMJNV vs MejoMJNV
-echo "Starting MeenMJNV vs MejoMJNV (LOSAT)..."
-(time $LOSAT_BIN tblastx -q ./fasta/MeenMJNV.fasta -s ./fasta/MejoMJNV.fasta -o ./losat_out/MeenMJNV.MejoMJNV.tlosatx.n8.out --query-gencode 1 --db-gencode 1 -n 8 ) &> ./losat_out/MeenMJNV.MejoMJNV.tlosatx.n8.log 
-
-# AvCLPV vs PsCLPV
-echo "Starting AvCLPV vs PsCLPV (LOSAT)..."
-(time $LOSAT_BIN tblastx -q ./fasta/AvCLPV.fasta -s ./fasta/PsCLPV.fasta -o ./losat_out/AvCLPV.PsCLPV.tlosatx.n8.out --query-gencode 1 --db-gencode 1 -n 8 )&>./losat_out/AvCLPV.PsCLPV.tlosatx.n8.log 
-
 # --- LOSATP Commands ---
+echo "Starting LOSATP commands..."
 run_losatp_case() {
     local query="$1"
     local subject="$2"
@@ -136,25 +91,72 @@ for losatp_case in "${LOSATP_CASES[@]}"; do
     run_losatp_case "$query" "$subject" "$stem"
 done
 
-<<COMMENTOUT
+echo "Finished LOSATP commands!"
 # --- TLOSATX Commands (Genetic Code: 4) ---
-echo "Starting NZ_CP006932 self..."
+echo "Starting TLOSATX commands..."
+
+# --- TLOSATX Commands (Genetic Code: 1) ---
+echo "Starting AP027280 self..."
+(time $LOSAT_BIN tblastx -q ./fasta/AP027280.fasta -s ./fasta/AP027280.fasta -o ./losat_out/AP027280.AP027280.tlosatx.n1.out --query-gencode 1 --db-gencode 1 -n 1)&>./losat_out/AP027280.AP027280.tlosatx.n1.log 
+(time $LOSAT_BIN tblastx -q ./fasta/AP027280.fasta -s ./fasta/AP027280.fasta -o ./losat_out/AP027280.AP027280.tlosatx.n8.out --query-gencode 1 --db-gencode 1 -n 8 )&>./losat_out/AP027280.AP027280.tlosatx.n8.log 
+
+# MjeNMV vs MelaMJNV
+echo "Starting MjeNMV vs MelaMJNV (TLOSATX)..."
+(time $LOSAT_BIN tblastx -q ./fasta/MjeNMV.fasta -s ./fasta/MelaMJNV.fasta -o ./losat_out/MjeNMV.MelaMJNV.tlosatx.n8.out --query-gencode 1 --db-gencode 1 -n 8 ) &> ./losat_out/MjeNMV.MelaMJNV.tlosatx.n8.log 
+
+# MelaMJNV vs PemoMJNVA
+echo "Starting MelaMJNV vs PemoMJNVA (TLOSATX)..."
+(time $LOSAT_BIN tblastx -q ./fasta/MelaMJNV.fasta -s ./fasta/PemoMJNVA.fasta -o ./losat_out/MelaMJNV.PemoMJNVA.tlosatx.n8.out --query-gencode 1 --db-gencode 1 -n 8 ) &> ./losat_out/MelaMJNV.PemoMJNVA.tlosatx.n8.log 
+
+# PemoMJNVA vs PeseMJNV
+echo "Starting PemoMJNVA vs PeseMJNV (TLOSATX)..."
+(time $LOSAT_BIN tblastx -q ./fasta/PemoMJNVA.fasta -s ./fasta/PeseMJNV.fasta -o ./losat_out/PemoMJNVA.PeseMJNV.tlosatx.n8.out --query-gencode 1 --db-gencode 1 -n 8 ) &> ./losat_out/PemoMJNVA.PeseMJNV.tlosatx.n8.log 
+
+# PeseMJNV vs PemoMJNVB
+echo "Starting PeseMJNV vs PemoMJNVB (TLOSATX)..."
+(time $LOSAT_BIN tblastx -q ./fasta/PeseMJNV.fasta -s ./fasta/PemoMJNVB.fasta -o ./losat_out/PeseMJNV.PemoMJNVB.tlosatx.n8.out --query-gencode 1 --db-gencode 1 -n 8 ) &> ./losat_out/PeseMJNV.PemoMJNVB.tlosatx.n8.log 
+
+# PemoMJNVB vs LvMJNV
+echo "Starting PemoMJNVB vs LvMJNV (TLOSATX)..."
+(time $LOSAT_BIN tblastx -q ./fasta/PemoMJNVB.fasta -s ./fasta/LvMJNV.fasta -o ./losat_out/PemoMJNVB.LvMJNV.tlosatx.n8.out --query-gencode 1 --db-gencode 1 -n 8 ) &> ./losat_out/PemoMJNVB.LvMJNV.tlosatx.n8.log 
+
+# LvMJNV vs TrcuMJNV
+echo "Starting LvMJNV vs TrcuMJNV (TLOSATX)..."
+(time $LOSAT_BIN tblastx -q ./fasta/LvMJNV.fasta -s ./fasta/TrcuMJNV.fasta -o ./losat_out/LvMJNV.TrcuMJNV.tlosatx.n8.out --query-gencode 1 --db-gencode 1 -n 8 ) &> ./losat_out/LvMJNV.TrcuMJNV.tlosatx.n8.log 
+
+# TrcuMJNV vs MellatMJNV
+echo "Starting TrcuMJNV vs MellatMJNV (TLOSATX)..."
+(time $LOSAT_BIN tblastx -q ./fasta/TrcuMJNV.fasta -s ./fasta/MellatMJNV.fasta -o ./losat_out/TrcuMJNV.MellatMJNV.tlosatx.n8.out --query-gencode 1 --db-gencode 1 -n 8 ) &> ./losat_out/TrcuMJNV.MellatMJNV.tlosatx.n8.log 
+
+# MellatMJNV vs MeenMJNV
+echo "Starting MellatMJNV vs MeenMJNV (TLOSATX)..."
+(time $LOSAT_BIN tblastx -q ./fasta/MellatMJNV.fasta -s ./fasta/MeenMJNV.fasta -o ./losat_out/MellatMJNV.MeenMJNV.tlosatx.n8.out --query-gencode 1 --db-gencode 1 -n 8 ) &> ./losat_out/MellatMJNV.MeenMJNV.tlosatx.n8.log 
+
+# MeenMJNV vs MejoMJNV
+echo "Starting MeenMJNV vs MejoMJNV (TLOSATX)..."
+(time $LOSAT_BIN tblastx -q ./fasta/MeenMJNV.fasta -s ./fasta/MejoMJNV.fasta -o ./losat_out/MeenMJNV.MejoMJNV.tlosatx.n8.out --query-gencode 1 --db-gencode 1 -n 8 ) &> ./losat_out/MeenMJNV.MejoMJNV.tlosatx.n8.log 
+
+# AvCLPV vs PsCLPV
+echo "Starting AvCLPV vs PsCLPV (TLOSATX)..."
+(time $LOSAT_BIN tblastx -q ./fasta/AvCLPV.fasta -s ./fasta/PsCLPV.fasta -o ./losat_out/AvCLPV.PsCLPV.tlosatx.n8.out --query-gencode 1 --db-gencode 1 -n 8 )&>./losat_out/AvCLPV.PsCLPV.tlosatx.n8.log 
+
+echo "Starting NZ_CP006932 self (TLOSATX)..."
 # NZ_CP006932 self
 (time $LOSAT_BIN tblastx -q ./fasta/NZ_CP006932.fasta -s ./fasta/NZ_CP006932.fasta -o ./losat_out/NZ_CP006932.NZ_CP006932.tlosatx.n8.out --query-gencode 4 --db-gencode 4 -n 8 )&>./losat_out/NZ_CP006932.NZ_CP006932.tlosatx.n8.log 
-echo "Starting AP027132 vs NZ_CP006932..."
+echo "Starting AP027132 vs NZ_CP006932 (TLOSATX)..."
 # AP027132 vs NZ_CP006932
 (time $LOSAT_BIN tblastx -q ./fasta/AP027132.fasta -s ./fasta/NZ_CP006932.fasta -o ./losat_out/AP027132.NZ_CP006932.tlosatx.n8.out --query-gencode 4 --db-gencode 4 -n 8 )&>./losat_out/AP027132.NZ_CP006932.tlosatx.n8.log 
-echo "Starting AP027078 vs AP027131..."
+echo "Starting AP027078 vs AP027131 (TLOSATX)..."
 # AP027078 vs AP027131
 (time $LOSAT_BIN tblastx -q ./fasta/AP027078.fasta -s ./fasta/AP027131.fasta -o ./losat_out/AP027078.AP027131.tlosatx.n8.out --query-gencode 4 --db-gencode 4 -n 8 )&>./losat_out/AP027078.AP027131.tlosatx.n8.log 
-echo "Starting AP027131 vs AP027133..."
+echo "Starting AP027131 vs AP027133 (TLOSATX)..."
 # AP027131 vs AP027133
 (time $LOSAT_BIN tblastx -q ./fasta/AP027131.fasta -s ./fasta/AP027133.fasta -o ./losat_out/AP027131.AP027133.tlosatx.n8.out --query-gencode 4 --db-gencode 4 -n 8 )&>./losat_out/AP027131.AP027133.tlosatx.n8.log 
-echo "Starting AP027133 vs AP027132..."
+echo "Starting AP027133 vs AP027132 (TLOSATX)..."
 # AP027133 vs AP027132
 (time $LOSAT_BIN tblastx -q ./fasta/AP027133.fasta -s ./fasta/AP027132.fasta -o ./losat_out/AP027133.AP027132.tlosatx.n8.out --query-gencode 4 --db-gencode 4 -n 8 )&>./losat_out/AP027133.AP027132.tlosatx.n8.log 
 
-COMMENTOUT
+echo "Finished TLOSATX commands!"
 
 
 
